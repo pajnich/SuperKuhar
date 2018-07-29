4f3567bf7f73bbba7ec12c8b1ce99ce8f228bad0
Matic P <matic.pajnic@gmail.com>
2018-07-29 14:16:19 +0200

    Added recipe details activity with sliding panel (not filled yet).

---
+++ app/src/main/java/com/example/matic/superkuhar/RecipeDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.matic.superkuhar;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+    }
+}
--- app/src/main/java/com/example/matic/superkuhar/RecipeFragment.java
+++ app/src/main/java/com/example/matic/superkuhar/RecipeFragment.java
@@ -1,7 +1,9 @@
 package com.example.matic.superkuhar;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,15 @@ public class RecipeFragment extends android.support.v4.app.Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_recipe, container, false);
+
+        final ConstraintLayout rootLayout = rootView.findViewById(R.id.recipe_root_layout);
+        rootLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRecipeDetailsActivity = new Intent(rootLayout.getContext(), RecipeDetailsActivity.class);
+                startActivity(goToRecipeDetailsActivity);
+            }
+        });
 
         return rootView;
     }
